9dd9e02abe5cddbd16809b4203845d6a93b069b0
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-03 15:11:18 +0100

    js_window object method_lev_1_4

---
+++ main.js
@@ -0,0 +1,47 @@
+// === ALERT === pop up fenster (Info gebend)
+// window.alert("Hallo Welt");
+
+
+// === PROMPT === (zum Data eingeben)
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+
+
+// === PROMPT === der zweite wert wird als placeholder ausgeführt..
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+
+// === CONFIRM === (zustimmung gefordert)
+// window.confirm("Stimmen Sie meiner Meinung zu?"); 
+
+
+
+// === über CONSOLE.LOG ===
+
+/* let alter = prompt( "Gib mir bitte dein Alter:" );
+console.log(alter); */
+
+
+// ALERT wert vorbestimmen
+let b = 5;
+let a = b * 5 -3;
+alert(a);
+
+let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+let grosse = "2,72m";
+alert(mann + grosse);
+
+/*   === SEITEN zum THEMA ===
+
+    https://www.w3schools.com/jsref/met_win_alert.asp
+
+    https://www.w3schools.com/jsref/met_win_prompt.asp
+
+
+    https://www.w3schools.com/jsref/met_win_confirm.asp
+
+
+*/
+
+
+
+
+
